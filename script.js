e6fd0724292f0e2e97e06bff0d3ec9c3facf220f
Mohammad Jawad <mohammad.jawad850@gmail.com>
2023-05-29 20:31:08 +0930

    add 3 buttons, add background color, add styling.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 initializeEtchASketch(16);
 
 
-function initializeEtchASketch(rowsAndCols=10) {
+function initializeEtchASketch(rowsAndCols = 10) {
 
-    if(isNaN(rowsAndCols) || rowsAndCols < 2 || rowsAndCols>100){
+    if (isNaN(rowsAndCols) || rowsAndCols < 2 || rowsAndCols > 100) {
         alert("please enter a valid number!");
         return;
     }
@@ -11,29 +11,60 @@ function initializeEtchASketch(rowsAndCols=10) {
     let container = document.querySelector('#container');
 
     container.innerHTML = "";
-  
+
     // let userInput = document.getElementById("sizeInput");
     // let rowsAndCols = parseInt(userInput.value);
-  
+
     for (let i = 0; i < rowsAndCols; i++) {
-      for (let j = 0; j < rowsAndCols; j++) {
-        const div = document.createElement('div');
-        div.classList.add("child");
-        container.append(div);
-      }
+        for (let j = 0; j < rowsAndCols; j++) {
+            const div = document.createElement('div');
+            div.classList.add("child");
+            container.append(div);
+        }
     }
-  
+
     container.style.gridTemplateColumns = `repeat(${rowsAndCols}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${rowsAndCols}, 1fr)`;
-  
+
     let childrenDivs = document.querySelectorAll(".child");
-  
-    for (let child of childrenDivs) {
-      child.addEventListener("mouseover", function () {
-        this.style.backgroundColor = "black";
-      });
-    }
-  }
-  
+
+    let bandw = document.querySelector("#blackandwhite");
+    let clear = document.querySelector("#clear")
+    // let randomNumber = Math.floor(Math.random() * 255) + 1;
+    let random = document.querySelector("#random");
+
+    function generateRandomNumber() {
+        return Math.floor(Math.random() * 255) + 1;
+      }
+
+
+    bandw.addEventListener("click", function () {
+        for (let child of childrenDivs) {
+            child.addEventListener("mouseover", function () {
+                this.style.backgroundColor = "black";
+            });
+        }
+    })
+
+    clear.addEventListener("click", function () {
+        for (let child of childrenDivs) {
+            child.addEventListener("mouseover", function () {
+                this.style.backgroundColor = "white";
+            });
+        }
+    })
+
+    random.addEventListener("click", function () {
+        for (let child of childrenDivs) {
+            child.addEventListener("mouseover", function () {
+                let red = generateRandomNumber();
+                let green = generateRandomNumber();
+                let blue = generateRandomNumber();
+                this.style.backgroundColor =   `rgb(${red}, ${green}, ${blue})`;
+            });
+        }
+    })
+}
+
   // Call the function to initialize the Etch a Sketch
 //   initializeEtchASketch();
